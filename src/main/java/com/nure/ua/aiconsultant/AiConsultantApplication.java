5449ac9cd31167f5b4b775ee5a16e56a091221a7
Xenia S <xenia.s@implemica.com>
2024-03-17 21:25:15 +0200

    Initial commit

---
+++ src/main/java/com/nure/ua/aiconsultant/AiConsultantApplication.java
@@ -0,0 +1,13 @@
+package com.nure.ua.aiconsultant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiConsultantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiConsultantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nure/ua/aiconsultant/AiConsultantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nure.ua.aiconsultant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiConsultantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
