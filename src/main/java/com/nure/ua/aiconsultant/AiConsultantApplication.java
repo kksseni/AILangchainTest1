af02017a3beea0797f9bc4ea5344a2cb2c5fb2a2
Xenia S <xenia.s@implemica.com>
2024-03-17 21:31:46 +0200

    formatting, set up gitignore

--- src/main/java/com/nure/ua/aiconsultant/AiConsultantApplication.java
+++ src/main/java/com/nure/ua/aiconsultant/AiConsultantApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AiConsultantApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AiConsultantApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AiConsultantApplication.class, args);
+    }
 
 }
--- src/test/java/com/nure/ua/aiconsultant/AiConsultantApplicationTests.java
+++ src/test/java/com/nure/ua/aiconsultant/AiConsultantApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AiConsultantApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
